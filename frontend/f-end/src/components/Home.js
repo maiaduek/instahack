cee9bae9956547b67efa573afde864847e45e3f8
Maia Duek <duekmaia@gmail.com>
2022-03-18 15:00:25 -0400

    home edit

--- frontend/f-end/src/components/Home.js
+++ frontend/f-end/src/components/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
             <h4 style={{fontWeight: "bold"}}>This nook of the social media world is dedicated to coders. </h4>
             <h5 className="card-text text-center mt-0" style={{paddingLeft: "50px", paddingRight: "50px"}}>Create an account and post your thoughts, read other coder's thoughts, and comment your thoughts about their thoughts. No need to keep things DRY here, leave that for your code and post as much as you like, run wild with repetition, post about current events or fiction, oh, and thoughts.</h5>
           </div>
-          <img src="https://media.istockphoto.com/vectors/happy-clients-giving-positive-feedback-to-product-quality-vector-id1250666724?k=20&m=1250666724&s=612x612&w=0&h=8OA4X0GxasHOl08QMa31odKbsTX3HlAtiN1j8YuYuEg=" className="card-img mt-5" style={{marginTop: "100px"}}/>
+          <img src="https://media.istockphoto.com/vectors/happy-clients-giving-positive-feedback-to-product-quality-vector-id1250666724?k=20&m=1250666724&s=612x612&w=0&h=8OA4X0GxasHOl08QMa31odKbsTX3HlAtiN1j8YuYuEg=" className="card-img mt-5" style={{marginTop: "100px", marginLeft: "50px", width: "85%"}}/>
           
         </div>
       </div>
