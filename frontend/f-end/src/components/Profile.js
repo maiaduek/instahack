f0c0d9e6b73f52ce49c831c2947f63e4b6d58a72
Maia Duek <duekmaia@gmail.com>
2022-03-30 17:14:53 -0400

    centered bio. started fixing number of comments on posts in profile

--- frontend/f-end/src/components/Profile.js
+++ frontend/f-end/src/components/Profile.js
@@ -67,7 +67,7 @@ function Profile(props) {
   const changeBio = (bioText) => {
     setUserInfo({...userInfo, bio: bioText})
   }
-  console.log("IMGA::", userInfo)
+
   return (
     <div>
       <nav className="navbar nav-pills bg-primary p-2 justify-content-end d-flex justify-content-between">
@@ -83,14 +83,18 @@ function Profile(props) {
       
       <div className="row">
         <img src={userInfo.image} className="col-3 mt-5 ms-5" style={{borderRadius: "200px"}}/>
-        <div className="col-6">
+        <div className="d-flex flex-column justify-content-center align-items-center col-6">
           <h2 className="mt-5">{userInfo.firstName} {userInfo.lastName}</h2>
           <h3 style={{color: "grey"}} className="mt-4">{userInfo.username}</h3>
           <h4 className="mt-4" style={{color: "grey"}}>Member since: {userInfo.createdAt?.slice(0,10)}</h4>
-          <div style={{display: "flex", flexDirection: "row", flexWrap: "wrap", width: "400px"}}>
-            { editing ? <textarea className="shadow" value={userInfo.bio} onChange={(e) => changeBio(e.target.value)}/> : <p>{userInfo.bio}</p> }
+          <div style={{display: "flex", flexDirection: "row", justifyContent: "center", flexWrap: "wrap", width: "400px"}}>
+            { 
+              editing ? 
+              <textarea className="shadow" value={userInfo.bio} onChange={(e) => changeBio(e.target.value)}/> : 
+              <p className="col-8 mt-3">{userInfo.bio}</p> 
+            }
             {loggedInUser._id === userInfo._id ? 
-            <svg xmlns="http://www.w3.org/2000/svg"  onClick={setUpdatedBio} width="16" height="16" fill="currentColor" className="bi bi-pencil-square ms-3 mt-1" viewBox="0 0 16 16" >
+            <svg xmlns="http://www.w3.org/2000/svg"  onClick={setUpdatedBio} width="16" height="16" fill="currentColor" className="bi bi-pencil-square ms-0 mt-3" viewBox="0 0 16 16" >
               <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
               <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
             </svg> : ''}
@@ -103,6 +107,7 @@ function Profile(props) {
       {
         userInfo?.posts?.length ? 
         userInfo.posts.map((post, i) => {
+          console.log(post.comments)
           return (
           <div className="card w-76 ms-5 mt-3 shadow" key={i}>
             <div className="card-body">
