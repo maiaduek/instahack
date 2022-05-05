983c0f697e59d925c9a74e4d130ad045c1609850
Maia Duek <duekmaia@gmail.com>
2022-05-05 15:27:38 -0400

    test2

--- frontend/f-end/src/http/url.js
+++ frontend/f-end/src/http/url.js
@@ -1,3 +1,3 @@
 // export const url = "http://localhost:5005";
-// export const url = "https://sleepy-escarpment-67306.herokuapp.com";
-export const url = "https://instahack.netlify.app";
+export const url = "https://sleepy-escarpment-67306.herokuapp.com";
+// export const url = "https://instahack.netlify.app";
